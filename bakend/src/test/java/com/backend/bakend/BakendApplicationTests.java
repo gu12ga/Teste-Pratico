a789fc3f51324def3233b9765c434fe3a1510cf3
gu12ga <gustavogabrielmartins55@gmail.com>
2024-03-05 22:33:56 -0300

    Criação do front e back

---
+++ bakend/src/test/java/com/backend/bakend/BakendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.bakend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
